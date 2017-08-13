9a6bac8ed29b6b78de7cceefed48a5705ecf5c83
MCL88 <miccel88@gmail.com>
2017-08-13 16:11:33 +0200

    Semplice Hello World! in GO

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import
+(
+	"fmt"
+)
+
+func main() {
+//Altro modo carino per indicare il tipo stringa in Go
+//Castare una stringa come Array di Byte
+	fmt.Println([]byte("Ciao!"))
+}
